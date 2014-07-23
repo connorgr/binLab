04b403e7348c9f9a4400b0b5e0cfbc981f1f7a43
connor <connor@cs.brown.edu>
2014-07-23 15:45:53 -0400

    Fix typo

--- ciede2000/pythonGrid.py
+++ ciede2000/pythonGrid.py
@@ -5,7 +5,7 @@ with open('../data/colorsTestPairs.csv', 'rb') as csvfile:
   reader = csv.reader(csvfile)
   pairs = []
   for r in reader:
-    pairs.append(r)
-    call(['clusterComputeDistance', r])
+    pairs.append(r[0])
+    call(['clusterComputeDistance', r[0]])
 
 print 'hello, world.'
